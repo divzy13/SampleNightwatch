40081bac9f0df692820c3cc4ccbd4028d5df69c1
divzy13 <divzy13@gmail.com>
2019-06-25 13:05:20 +0200

    Open Google and Search

---
+++ tests/firstTest.js
@@ -0,0 +1,15 @@
+module.exports = {
+    'Demo Google page' : function(client) {
+        client
+        .url("http://google.co")
+        .waitForElementVisible('body', 1000)
+        .assert.title('Google')
+        .assert.visible('input[type=text]')
+        .setValue('input[type=text]' , 'Nightwatch')
+        //.click('input[type=text]')
+        .waitForElementVisible('input[name=btnK]', 2000)
+        .click('input[name=btnK]')
+        .pause(4000)
+        .end()
+    }
+}
