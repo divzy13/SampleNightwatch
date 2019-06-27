e082bd0840bc8e39298a95c2127fddb6539c29ba
divzy13 <divzy13@gmail.com>
2019-06-27 11:29:53 +0200

    Added new js file for CGI webpage

--- tests/CGIpage.js
+++ tests/CGIpage.js
@@ -2,7 +2,9 @@ module.exports = {
     'CGI Web page' : function(client) {
         client
         .url('https://www.cgi.com/en')
-        .waitForElementVisible('a:contains(Services)', 1000)
+        .waitForElementVisible('a[class=is-active]', 1000)
+        .click('a[class=is-active]')
+        .pause(5000)
         .end()
     }
 }
